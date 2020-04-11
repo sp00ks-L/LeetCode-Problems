0f7b8fe5fcd0ba5dc08fea0910567a46faaf437f
sp00ks-L <55932367+sp00ks-L@users.noreply.github.com>
2020-04-11 11:28:33 +0100

    diameterOfBinaryTree.py

---
+++ diameterOfBinaryTree.py
@@ -0,0 +1,24 @@
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution:
+    def diameterOfBinaryTree(self, root):
+        # Finds diameter recursively 
+        self.ans = 0
+
+        def depth(node):
+            if not node:
+                return 0
+            left = depth(node.left)
+            right = depth(node.right)
+            self.ans = max(self.ans, left + right)
+            return max(left, right) + 1
+
+        depth(root)
+        return self.ans
+
+
